7c6edfcc279a3ce6c12c74314501260a4f9e4ad8
sunnyeun <36465256+sunnyeun@users.noreply.github.com>
2022-10-24 21:38:07 -0400

    Module 7 Assignment
    Module 7 Assignment

---
+++ parts/navbar.php
@@ -0,0 +1,17 @@
+<header class="navbar">
+	<div class="container display-flex">
+		<div class="flex-none">
+			<h1>Store</h1>
+		</div>
+
+	<dix class="flex-stretch"></dix>
+	<div class="flex-none nav nav-flex">
+		<ul>
+			<li><a href="index.php"></a>Home</li>
+			<li><a href="product_list.php"></a>Product List</li>
+			<li><a href="about.php"></a>About Us</li>
+		</ul>
+	</div>
+
+	</div>
+</header>
---
+++ product_item.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+	<title>Store Product Item</title>
+	<? include "parts/meta.php"; ?>
+
+</head>
+
+<body>
+
+	<? include "parts/navbar.php"; ?>
+
+	<div class="container">
+		<div class="card soft">
+			<p>This is Item #<?= $_GET['id'] ?></p>
+		</div>
+	</div>
+
+</body>
+
+</html>
